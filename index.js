4d42592d3bd0af15ccaad4c8114b56c68a4fdbe5
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-07-06 14:08:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
 let randomNumber = Math.floor(Math.random() * 9 + 1);
-let guessNumber = await inquirer.prompt([
-    {
-        message: "Enter Number 1-to-10 Try Your Luck",
-        type: "number",
-        name: "guessNumber"
+let condition = true;
+while (condition) {
+    let guessNumber = await inquirer.prompt([
+        {
+            message: "Enter Number 1-to-10 Try Your Luck",
+            type: "number",
+            name: "guessNumber"
+        }
+    ]);
+    if (guessNumber.guessNumber === randomNumber) {
+        console.log("\n Congratulation Your Guess Right, You Are Winner! ;) \n");
     }
-]);
-console.log(guessNumber);
-if (guessNumber.guessNumber === randomNumber) {
-    console.log("\n Congratulation You Are Winner! ;) \n");
-}
-else if (guessNumber.guessNumber > randomNumber) {
-    console.log("Sorry Your Guess NUmber Is bigger! Please try again :( ", randomNumber, " < The Random Number");
-}
-else if (guessNumber.guessNumber < randomNumber) {
-    console.log("Sorry Your Guess Number is Lower! Please Try Again :( ", randomNumber, " < The Random Number");
-}
-else {
-    console.log("Not Any Number Define!, First You Choose a number");
+    else if (guessNumber.guessNumber > randomNumber) {
+        console.log("\nSorry Your Guess NUmber Is bigger! Please try again :( ", randomNumber, " < The Random Number\n");
+    }
+    else if (guessNumber.guessNumber < randomNumber) {
+        console.log("\nSorry Your Guess Number is Lower! Please Try Again :( ", randomNumber, " < The Random Number\n");
+    }
+    else {
+        console.log("\nNot Any Number Define!, First You Choose a number\n");
+    }
+    let GameOver = await inquirer.prompt([
+        {
+            message: "Do You Want To Play Again?",
+            type: "confirm",
+            name: "playAgain",
+            default: true,
+        }
+    ]);
+    condition = GameOver.playAgain;
 }
