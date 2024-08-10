33d89e689e8d375f8a567f9716f5c8f0f2525a78
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-11 01:39:50 +0500

    add user name fanctionality

--- index.ts
+++ index.ts
@@ -3,37 +3,26 @@
 import inquirer from 'inquirer'
 
 let randomNumber = Math.floor(Math.random() * 9 + 1)
-let condition = true;
-
-while (condition) {
-    let guessNumber = await inquirer.prompt([
-        {
-            message: "Enter Number 1-to-10 Try Your Luck",
-            type: "number",
-            name: "guessNumber"
-        }
-    ])
-
-    if (guessNumber.guessNumber === randomNumber) {
-        console.log("\n Congratulation Your Guess Right, You Are Winner! ;) \n");
-    } else if (guessNumber.guessNumber > randomNumber) {
-        console.log("\nSorry Your Guess NUmber Is bigger! Please try again :( ", randomNumber, " < The Random Number\n");
+let userName = await inquirer.prompt({
+    name: "user",
+    type: "input",
+    message: "What is your name?",
+})
+let guessNumber = await inquirer.prompt([
+    {
+        message: "Enter Number 1-to-10 Try Your Luck",
+        type: "number",
+        name: "guessNumber"
     }
-    else if (guessNumber.guessNumber < randomNumber) {
-        console.log("\nSorry Your Guess Number is Lower! Please Try Again :( ", randomNumber, " < The Random Number\n");
-    } else {
-        console.log("\nNot Any Number Define!, First You Choose a number\n");
-    }
-
-    let GameOver = await inquirer.prompt([
-        {
-            message: "Do You Want To Play Again?",
-            type: "confirm",
-            name: "playAgain",
-            default: true,
-        }
-    ])
-    condition = GameOver.playAgain
+])
+if (guessNumber.guessNumber === randomNumber) {
+    console.log(`\n Congratulation ${userName.user} Your Guess Right, You Are Winner! ;) \n`);
+} else if (guessNumber.guessNumber > randomNumber) {
+    console.log(`\nSorry ${userName.user} Guess NUmber Is bigger! Please try again :( \n  ${randomNumber} < The Random Number\n`);
 }
-
+else if (guessNumber.guessNumber < randomNumber) {
+    console.log(`\nSorry ${userName.user} Guess Number is Lower! Please Try Again :(  \n ${randomNumber}  < The Random Number\n`);
+} else {
+    console.log(`\n Not Any Number Define!, you should Choose a number\n`);
+}
 
